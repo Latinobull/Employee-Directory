3c566528be3f7f564cd43158ea1492ceb6d5c140
Donnahue George <Donnahuegjr@gmail.com>
2021-03-26 11:21:31 -0400

    added table to the page

--- src/components/main.js
+++ src/components/main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './header';
 import Search from './search';
+import Table from './table';
 function Main() {
   return (
     <div>
       <Header />
       <Search />
+      <Table />
     </div>
   );
 }
---
+++ src/components/table.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Table(props) {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">Image</th>
+          <th scope="col">Name</th>
+          <th scope="col">Phone</th>
+          <th scope="col">Email</th>
+          <th scope="col">DOB</th>
+        </tr>
+      </thead>
+
+      <tbody>{props.tableRow}</tbody>
+    </table>
+  );
+}
+
+export default Table;
