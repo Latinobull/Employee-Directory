a5cff043fc0f9eb6efcaeba04ec50b391f829a03
Donnahue George <Donnahuegjr@gmail.com>
2021-03-29 19:24:31 -0400

    using ui-kit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'uikit/dist/css/uikit.min.css';
 ReactDOM.render(
   <React.StrictMode>
     <App />
